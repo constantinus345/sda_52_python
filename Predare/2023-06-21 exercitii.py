7470917e87e4c598b31f78bde46eccde246c325d
constantinus345 <96290754+constantinus345@users.noreply.github.com>
2023-06-21 21:00:46 +0300

    21 iunie

--- Predare/2023-06-21 exercitii.py
+++ Predare/2023-06-21 exercitii.py
@@ -14,3 +14,9 @@ print(lista_mea_clasic)
 #Daca vreau ca patratele sa fie intre 1 si 100 inclusiv, divizibil cu 3
 lista_patrate_100 = [n**2 for n in range(1,101) if (n**2 <= 100) and (n % 3 == 0)]
 print(lista_patrate_100)
+
+#3. Creati o lista cu lungimea fiecarui cuvant dintr-un string
+propozitia = "Ce mai faci mai Ionele"
+list_cuv = [len(cuv) for cuv in propozitia.split()]
+#propozitia.split() imi extrage fiecare cuvant din propozitie
+print(list_cuv)
