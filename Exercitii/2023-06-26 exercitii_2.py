3866bc644a342f71b4a9784de90f878971eeb623
constantinus345 <96290754+constantinus345@users.noreply.github.com>
2023-06-26 20:02:12 +0300

    primul mesaj

---
+++ Exercitii/2023-06-26 exercitii_2.py
@@ -0,0 +1,36 @@
+"""
+Functii
+Creează o funcție care primește un șir de caractere și returnează un dicționar care conține numărul de apariții ale fiecărui caracter din șir.
+Creează o funcție care primește două liste și returnează o listă care conține elementele comune din cele două liste.
+Creează o funcție care primește un numar și returnează o listă care conține toate perechile de numere din listă care au produsul egal cu un număr dat. Ex: pentru 12 avem [(1,12), (2,6), (3,4)]
+Creează o funcție care primește un șir de caractere și returnează adevărat dacă șirul este format din caractere consecutive în ordine alfabetică și fals în caz contrar. De exemplu ars - True, arde - False.
+Scrieti o functie care accepta ca parametri o lista de numere si un numar, iar rezultatul sa fie produsul fiecarui element din lista + nr. De exemplu pentru [2,3,4] si numarul 5 avem rezultatul = (2+5)(3+5)*(4+5)
+Scrieti o functie care calculeaza factorialul unui numar
+Scrieti o functie care calculeaza al n-lea numar in sirul lui Fibonacci. (Șirul lui Fibonacci este un șir de numere în care fiecare număr este suma celor două numere anterioare. Șirul începe cu 1 și 1, iar primele câteva numere din șir sunt: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...)
+Scrieti o functie care calculeaza sin(x)+cos(x)
+Scrieti o functie care verifica daca un numar e patrat perfect
+Scrieti o functie care primeste o lista de nume si returneaza o alta lista cu nume unice ordonate alfabetic.
+Scrieti o functie care verifica daca un string e palindrom (citit in ambele directii e acelasi, de exemplu ana e palindrom)
+
+12. Scrieti un program care ghiceste un numar, zicand daca esti foarte departe, departe sau foarte aproape de numarul de ghicit
+13.
+Creati un program care scrie cate secunde au mai ramas, gen countdown timer.
+Hint: folositi from time import sleep
+14.
+Aveti 2 liste de nume: studenti inscrisi la curs si studenti prezenti azi.
+Creati o functie care face prezenta.
+(Nu dau instructiuni detaliate, va las sa fiti creativi)
+15.
+Creati o functie care valideaza un email (daca string-ul respectiv e valid pentru a fi considerat email)
+16.
+Aveti o un dictionar cu nume si varsta, fiecare are cate 4 elemente.
+Creati un fisier care sa contina text cu numele si varsta fiecarei persoane, in modul urmator, exemplu:
+Andrei are 20 ani, deci e nascut in 2003
+Ion are...
+...
+...
+17.
+Aveti acest fisier cu notele a 3 elevi.
+Creati alt fisier care scrie media aritmetica a fiecarui elev.
+
+"""
