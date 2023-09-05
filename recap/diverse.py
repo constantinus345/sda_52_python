13336c7f03233b732b9933d9acf2e59cd2e51341
constantinus345 <96290754+constantinus345@users.noreply.github.com>
2023-09-05 19:36:35 +0300

    5 sep

--- recap/diverse.py
+++ recap/diverse.py
@@ -20,9 +20,12 @@ lista = ['1h2nhb-98', 'hfsd755']
 lista_suma = [suma_cifrelor(x) for x in lista]
 print(lista_suma)
 
-
+#pattern matching l;a cuvintele care ioncep cu A sau B
 text_localitati = "Anul acesta am vizitat Brasov, Bucuresti, Iasi si Arad"
 pattern_ab = r'\b[A|B]\w+\b'
+#\b incepe sau se termina cu...
+#[A|B] A sau B, un singur caracter by default = [A|B]{1}
+#\w+ orice caracter a-Z sau 0-9, + inseamna 1+ repetari
 
 matches = re.findall(pattern_ab, text_localitati)
 print(matches)
